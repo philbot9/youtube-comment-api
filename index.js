291aa389dcd9203f9913c7316fbd4c4547483ba4
Philip Klostermann <philip.klostermann@gmail.com>
2015-09-29 09:38:24 -0400

    support promises and callbacks

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+var _ = require('lodash');
 var getCommentPage = require('lib/comment-pager');
 
 module.exports = function (videoID, pageToken, cb) {
+  var useCallback = cb && _.isFunction(cb);
   if (!videoID) {
-    throw new Error('Cannot fetch comment page. No videoID provided.');
+    var e = new Error('Cannot fetch comment page. No videoID provided.');
+    if (useCallback) {
+      return cb(e);
+    }
+    throw e;
   }
   getCommentPage(videoID, pageToken).then(function (page) {
     if (!page) {
-      throw new Error('Error fetching comment page from YouTube.');
+      var e = new Error('Error fetching comment page from YouTube.');
+      if (useCallback) {
+        return cb(e);
+      }
+      throw e;
     }
-    return {
+
+    var commentPage = {
       pageToken: pageToken,
       nextPageToken: page.nextPageToken,
       videoCommentCount: page.videoCommentCount,
       comments: page.comments
     };
+
+    if (useCallback) {
+      return cb(null, commentPage);
+    }
+    return commentPage;
   });
 };
