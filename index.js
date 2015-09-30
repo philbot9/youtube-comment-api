2448777bc1bdb5adbe5ccd2eae6446dd97290fe1
Philip Klostermann <philip.klostermann@gmail.com>
2015-09-30 16:37:16 -0400

    add tests

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 var _ = require('lodash');
-var getCommentPage = require('lib/comment-pager');
+var debug = require('debug')('youtube-comment-api');
+var getCommentPage = require('./lib/comment-pager');
 
 module.exports = function (videoID, pageToken, cb) {
-  var useCallback = cb && _.isFunction(cb);
+  var useCallback = !!cb && _.isFunction(cb);
   if (!videoID) {
-    var e = new Error('Cannot fetch comment page. No videoID provided.');
+    var e = new Error('Cannot fetch comment page. No video ID provided.');
     if (useCallback) {
       return cb(e);
     }
     throw e;
   }
-  getCommentPage(videoID, pageToken).then(function (page) {
+
+  var pendingPromise = getCommentPage(videoID, pageToken).then(function (page) {
     if (!page) {
-      var e = new Error('Error fetching comment page from YouTube.');
-      if (useCallback) {
-        return cb(e);
-      }
-      throw e;
+      throw new Error('Error fetching comment page from YouTube.');
     }
 
-    var commentPage = {
+    return _.defaults({
       pageToken: pageToken,
-      nextPageToken: page.nextPageToken,
-      videoCommentCount: page.videoCommentCount,
-      comments: page.comments
-    };
-
-    if (useCallback) {
-      return cb(null, commentPage);
-    }
-    return commentPage;
+      nextPageToken: page.nextPageToken
+    }, page);
   });
+
+  if (useCallback) {
+    pendingPromise.then(function (commentPage) {
+      cb(null, commentPage);
+    }).catch(function (err) {
+      cb(err);
+    });
+    return;
+  }
+  return pendingPromise;
 };
---
+++ test/youtube-comment-api.js
@@ -0,0 +1,123 @@
+var expect = require('chai').expect;
+var fetchCommentPage = require('../index');
+
+describe('YouTube Comment API', function () {
+  it('should export a function', function () {
+    expect(require('../index')).to.be.a('function');
+  });
+
+  it('should support callback functions', function (done) {
+    this.timeout(20000);
+    fetchCommentPage('pkwOrteyQtY', null, function (err, page) {
+      expect(err).not.to.exist;
+      expect(page).to.exist;
+      expect(page).to.have.a.property('videoTitle').which.is.a('string');
+      expect(page).to.have.a.property('comments').which.is.a('array');
+      expect(page.comments).to.have.length.above(0);
+      done();
+    });
+  });
+
+  it('should return errors for callback functions', function (done) {
+    this.timeout(20000);
+    fetchCommentPage('fakeID', null, function (err, page) {
+      expect(err).to.exist;
+      expect(page).not.to.exist;
+      done();
+    });
+  });
+
+  it('should support promises', function (done) {
+    this.timeout(20000);
+    fetchCommentPage('pkwOrteyQtY', null).then(function (page) {
+      expect(page).to.exist;
+      expect(page).to.have.a.property('videoTitle').which.is.a('string');
+      expect(page).to.have.a.property('comments').which.is.a('array');
+      expect(page.comments).to.have.length.above(0);
+      done();
+    });
+  });
+
+  it('should return errors for promises', function (done) {
+    this.timeout(20000);
+    fetchCommentPage('fakeID', null).catch(function(err) {
+      expect(err).to.exist;
+      done();
+    });
+  });
+
+  it('should throw an error if no video ID is provided', function () {
+    expect(fetchCommentPage).to.throw(Error);
+    expect(function () {
+      fetchCommentPage(null, null);
+    }).to.throw(Error);
+  });
+
+  it('should get a comments page without a page token', function (done) {
+    this.timeout(20000);
+    fetchCommentPage('pkwOrteyQtY', null).then(function (page) {
+      expect(page).to.have.a.property('comments').that.is.an('array');
+      expect(page.comments).to.have.length.above(1);
+      expect(page).to.have.a.property('nextPageToken').that.is.a('string');
+      expect(page.nextPageToken).to.have.length.above(5);
+
+      page.comments.forEach(function (c) {
+        expect(c).to.have.a.property('id').that.is.a('string');
+        expect(c).to.have.a.property('user').that.is.a('string');
+        expect(c).to.have.a.property('date').that.is.a('string');
+        expect(c).to.have.a.property('timestamp').that.is.a('number');
+        expect(c).to.have.a.property('commentText').that.is.a('string');
+        expect(c).to.have.a.property('likes').that.is.a('number');
+        expect(c).to.have.a.property('hasReplies').that.is.a('boolean');
+        if (c.hasReplies) {
+          expect(c).to.have.a.property('replies').that.is.an('array');
+          expect(c.replies).to.have.length.above(0);
+        }
+      });
+      done();
+    });
+  });
+
+  it('should get a different comments page with a page token', function (done) {
+    this.timeout(20000);
+    fetchCommentPage('pkwOrteyQtY', null).then(function (page1) {
+      fetchCommentPage('pkwOrteyQtY', page1.nextPageToken).then(function (page2) {
+        expect(page1).to.not.deep.equal(page2);
+        expect(page2).to.have.a.property('comments').that.is.an('array');
+        expect(page2.comments).to.have.length.above(1);
+        expect(page2).to.have.a.property('nextPageToken').that.is.a('string');
+        expect(page2.nextPageToken).to.have.length.above(5);
+
+        page2.comments.forEach(function (c) {
+          expect(c).to.have.a.property('id').that.is.a('string');
+          expect(c).to.have.a.property('user').that.is.a('string');
+          expect(c).to.have.a.property('date').that.is.a('string');
+          expect(c).to.have.a.property('timestamp').that.is.a('number');
+          expect(c).to.have.a.property('commentText').that.is.a('string');
+          expect(c).to.have.a.property('likes').that.is.a('number');
+          expect(c).to.have.a.property('hasReplies').that.is.a('boolean');
+          if (c.hasReplies) {
+            expect(c).to.have.a.property('replies').that.is.an('array');
+            expect(c.replies).to.have.length.above(0);
+          }
+        });
+
+        done();
+      });
+    });
+  });
+
+  it('should include video information', function (done) {
+    this.timeout(20000);
+    fetchCommentPage('pkwOrteyQtY', null).then(function (page) {
+      expect(page).to.exist;
+      expect(page).to.have.a.property('videoCommentCount').that.is.a('number');
+      expect(page.videoCommentCount).to.be.above(0);
+      expect(page).to.have.a.property('videoTitle').that.is.a('string');
+      expect(page.videoTitle.length).to.be.above(0);
+      expect(page).to.have.a.property('videoThumbnail').that.is.a('string');
+      expect(page.videoThumbnail.length).to.be.above(0);
+      done();
+    });
+  });
+});
